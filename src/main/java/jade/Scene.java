c13cb375c978f2d9aafd8ee31a21da5535d62297
Conor Keegan <conkeegs@gmail.com>
2022-04-24 19:32:56 -0400

    GameObjects and Components created

--- src/main/java/jade/Scene.java
+++ src/main/java/jade/Scene.java
@@ -1,7 +1,12 @@
 package jade;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Scene {
     protected Camera camera;
+    private boolean isRunning = false;
+    protected List<GameObject> gameObjects =  new ArrayList<>();
 
     public Scene() {
 
@@ -9,6 +14,22 @@ public abstract class Scene {
 
     public void init() {
 
+    }
+
+    public void start() {
+        for (GameObject gameObject : gameObjects) {
+            gameObject.start();
+        }
+
+        isRunning = true;
+    }
+
+    public void addGameObjectToScene(GameObject gameObject) {
+        gameObjects.add(gameObject);
+
+        if (isRunning) {
+            gameObject.start();
+        }
     }
 
     public abstract void update(float dt);
